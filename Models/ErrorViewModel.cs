0395b0a243d0de6094655caa24291077022367c5
Rodrigo Valdes <rodrigovaldes89@gmail.com>
2018-06-08 23:57:26 -0300

    commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prueba_gitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
